caaf1ad151ab2b0ff37915f326af288e3c18ad17
test app for rails <tainguyentgmail.com>
2016-01-08 01:34:24 +0700

    create 2 table chef and recipes for use later

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160107175026) do
+ActiveRecord::Schema.define(version: 20160107183055) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title",      limit: 255
@@ -21,10 +21,25 @@ ActiveRecord::Schema.define(version: 20160107175026) do
     t.integer  "user_id",    limit: 4
   end
 
+  create_table "chefs", force: :cascade do |t|
+    t.string   "chefname",   limit: 255
+    t.string   "email",      limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "comments", force: :cascade do |t|
     t.text     "comments",   limit: 65535
     t.text     "body",       limit: 65535
     t.integer  "article_id", limit: 4
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "recipes", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.text     "summary",     limit: 65535
+    t.text     "description", limit: 65535
     t.datetime "created_at"
     t.datetime "updated_at"
   end
