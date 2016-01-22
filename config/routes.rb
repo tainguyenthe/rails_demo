92a4d24105e422c77e1a2d3bfceba47ed76da600
test app for rails <tainguyentgmail.com>
2016-01-22 21:43:28 +0700

    lecture-32
    lecture 32

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
-  get 'homes/show'
-  
-  get 'homes/index'
-
-  
+  # get 'homes/show'
+  # get 'homes/index'
 
   #root 'blog#index'
   #resources :articles
   
   root 'page#home'
+
   get '/home', to: 'page#home'
+
+  # get '/recipes', to: 'recipes#index'
+  # get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
+  # post '/recipes/:id', to: 'recipes#create'
+  # get '/recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
+  # patch '/recipes/:id', to: 'recipes#update'
+  # get 'recipes/:id', to: 'recipes#show', as: 'recipe'
+  # delete '/recipes/:id', to: 'recipes#destroy'
+  resources	:recipes
 end
